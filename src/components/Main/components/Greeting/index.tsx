fc4e822e3c77730ba26971a161b34d8f128808e0
pas <whyiampas@gmail.com>
2021-08-01 02:22:55 +0300

    added hover sd scale animation to svg

--- src/components/Main/components/Greeting/index.tsx
+++ src/components/Main/components/Greeting/index.tsx
@@ -9,6 +9,33 @@ const useStyles = makeStyles(({ palette: { background } }) => ({
       background: background.default,
       overflowX: 'hidden'
     },
+
+    '@keyframes racket': {
+      '0%': {
+        transform: 'scale3d(1,1,1)'
+      },
+
+      '30%': {
+        transform: 'scale3d(0.75,1.25,1)'
+      },
+      '40%': {
+        transform: 'scale3d(1.15,0.85,1)'
+      },
+      '50%': {
+        transform: 'scale3d(0.95,1.05,1)'
+      },
+      '65%': {
+        transform: 'scale3d(1.1,0.8,1)'
+      },
+      '75%': {
+        transform: 'scale3d(1.05,0.95,1)'
+      },
+
+      '100%': {
+        transform: 'scaleY(1)'
+      }
+    },
+
     '@keyframes lines': {
       '0%': {
         opacity: 0
@@ -33,6 +60,11 @@ const useStyles = makeStyles(({ palette: { background } }) => ({
     height: '100vh',
 
     '& .pasSvgContainer': {
+      '&:hover': {
+        animation: 'racket 1s '
+
+        // transform:'scaleY(0.4)'
+      },
       '& path': {
         fill: 'none',
         strokeLinecap: 'round',
