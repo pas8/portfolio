bc416a001ef5287a3590d94d40b799845c19a92f
pas <whyiampas@gmail.com>
2021-08-09 01:32:25 +0300

    debugging in prod

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -17,7 +17,7 @@ export default (req: any, res: any) => {
     subject: `Message From ${ req.body.name || req.body.email } `,
     text: req.body.title + req.body.message + ' | Sent from: ' + req.body.email,
   };
-console.log(mailData)
+console.log(mailData,PASSWORD)
   transporter.sendMail(mailData, (err: any) => {
     if (err) res.send('error',err);
   });
