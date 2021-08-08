606c6b98c60e19abcbccb05da861aae84c75b14c
pas <whyiampas@gmail.com>
2021-08-09 01:41:49 +0300

    trying to fix...

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,15 +1,31 @@
-export default (req: any, res: any) => {
-  let nodemailer = require('nodemailer');
 
-  const transporter = nodemailer.createTransport({
+const nodemailer = require("nodemailer");
+
+export default async (req: any, res: any) => {
+
+
+const transporter = nodemailer.createTransport({
     port: 465,
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     auth: {
-      user: process.env.REQ_EMAIL,
-      pass: process.env.PASSWORD
+        user: "myEmail@gmail.com",
+        pass: "password",
     },
-    secure: true
-  });
+    secure: true,
+});
+
+await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
 
   const mailData = {
     user: process.env.REQ_EMAIL,
@@ -17,9 +33,19 @@ export default (req: any, res: any) => {
     subject: `Message From ${ req.body.name || req.body.email } `,
     text: req.body.title + req.body.message + ' | Sent from: ' + req.body.email,
   };
-console.log(mailData,process.env.PASSWORD)
-  transporter.sendMail(mailData, (err: any) => {
-    if (err) res.send('error',err);
-  });
-  res.send('success');
+
+await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
+
+res.status(200).json({ status: "OK" });
 };
