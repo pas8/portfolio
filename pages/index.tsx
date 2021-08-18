58511072fee8e7dd9209b4d9f194c779da084ea3
pas <whyiampas@gmail.com>
2021-08-18 17:11:14 +0300

    deleted bug seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,9 @@
 import { FC } from 'react';
 import Main from 'components/Main';
 import { BlogDataType } from 'models/types';
-import { Head } from 'next/document';
 const Index: FC<{ blogArr: any }> = ({ blogArr }) => {
   return (
     <>
-      <Head>
-        <title>Pas portfolio</title>
-        <meta
-          name="description"
-          content="  I am Anatolii Ponocheniuk, self-educated by realizing studying projects and also acquire new info by reading the official documentation. And about my soft skills, I improved them in MBA school and Gdansk business week of course all communication was English. Exactly want to evolve as a react-ts developer and of course be fond of coding and right react with typescript. So, I have default hobbies. Certainly motivated enough to work in a new company."
-        />
-        <meta property="og:title" content="Anatolii Ponocheniuk Portfolio" />
-        <meta
-          property="og:description"
-          content="  I am Anatolii Ponocheniuk, self-educated by realizing studying projects and also acquire new info by reading the official documentation. And about my soft skills, I improved them in MBA school and Gdansk business week of course all communication was English. Exactly want to evolve as a react-ts developer and of course be fond of coding and right react with typescript. So, I have default hobbies. Certainly motivated enough to work in a new company."
-        />
-        <meta property="og:type" content="website" />
-      </Head>
-
       <Main blogArr={blogArr} />
     </>
   );
